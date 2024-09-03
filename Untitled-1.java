ccab67e66d8a2df217112defa3f004e9cdd0efe5
TaisaHenrique <tainataisa876@gmail.com>
2024-09-02 21:36:58 -0300

    Add files via upload
    commit segunda at

---
+++ Untitled-1.java
@@ -0,0 +1,23 @@
+
+public class processarString { 
+    public static void 
+    main ( String [] args) { 
+
+        string entrada = " caractere, vogaias, maiuscula, menuscula"; 
+
+        int numCaractere = entreda.length( );
+system.out.println(" numere de caractere: " + numCaractere);
+
+        string stringMaiuscula = entrada.toUpperCase( );
+system.out.println( " string em maiuscula : " + stringMaiuscula);
+
+        int numVogais = contarVogais ( entreda);
+system.out.println ( " numero de vogais :" + numVogais);
+        
+        boolean comecaComUni = entreda.toUpperCase().startsWith("UNI");
+system.out.println ( comeca com 'UNI': " + comecaComUni");
+
+        boolean terminaComRio = entreda.toUpperCase().endsWith ("RIO");
+system.out.println("Termina com 'RIO': + terminaComRio");
+    }
+   } 
